c2e9dd65e809759ca7f44deaca4ac187894971a9
Abhay Arora <abhayarorasap123@gmail.com>
2022-09-08 02:37:14 +0530

    Added props to map

--- components/Map/GoogleMapComponent.js
+++ components/Map/GoogleMapComponent.js
@@ -5,49 +5,54 @@ import { Map, Marker, GoogleApiWrapper, Polyline } from "google-maps-react";
 /*global google*/
 
 const customizeMap = {
-  width: "38.4%",
-  height: "37.6%",
+  width: "100%",
+  height: "100%",
 };
 
 var polyline_coords = [];
-
+var destinations = [];
 class GoogleMapComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //   cords: [
-      //     { latitude: 12.840315, longitude: 80.15305 },
-
-      //     { latitude: 31.046051, longitude: 34.851612 },
-      //     // { latitude: 51.165691, longitude: 10.451526 },
-      //     // { latitude: 52.215933, longitude: 19.134422 },
-      //     // { latitude: 50.0874654, longitude: 14.4212535 },
-      //     // { latitude: 7.5554942, longitude: 80.7137847 },
-      //   ],
       polyline_coords: [],
+      destinations: [],
     };
     var latlngs = polyUtil.decode(
       "{}jmAc}ehNhCv@vKeKhEkAxBW`BoA|NwPlh@wl@xlAcuAnl@ct@`QoStXmc@rG}KfMs_@|Moa@fCiPpEuY|N_ZdJrBpCr@`ErA?iAEcB@MJMbH_@lAwA~@oDF}Bz@uAlBcGjDkUHiF[{AkAgAaHqEl@_Nj@w`@}AmMhA}IWgB{@yBy@q@q@KjAc@lGeCxRuIhNsFtJ]pNJ~M|Cl_@nJb^rIfjAjZvl@zPpTvHzTlGlp@fRfo@dSpo@pQ`KvCvSdCdJzAzT|Gvk@xQlc@`Nnn@fPbZdIzG~@vLt@zMdEdIrBfJh@t\\nBdLnAzGfB`P~F~RlKv]fOxIpExMdFle@lOzn@nNhGfB`Fn@lN^vIQnCu@hC@cDA{CfAmJ?mOm@iHwAmAMi^qIiKqBsO{E_g@uPwWqMgLmEkRyJiY{KyQgCid@kCmKoB{JmDmEy@}N_AoYmHop@}PkkA{^qXoImJ}BaUeC{MoDso@qQ_o@cSgl@qPc`@aLgP}Fgy@eU_aAaWi{@}SgJuBwFQiLAcGd@cV`K_QpHTlBn@bDmAbL`BhM]vS{@rZzH~Et@dA\\pBSxEs@tGiCbNcBlEw@zACvBgA|DeAz@gHb@AdEpP|G}@O_EeBmDeBiQeFcFoAqA|ByLrVaCvOqDbW}HrV{Rpl@sClFaLnQyOzV}E~FcX|[eLnN}PtSmjAjrAcd@hh@yMjOcK`KuD\\}C`BiW~UsHpGiGnBkEvAs@tCqAnGcDjHkEtIkFjIuNfNqUxT_WxUcEbEcDlEgTbu@{Kv_@eT|s@_B`MkHly@}Kbb@{D~KwI|NaIlY{AzG[z@wEqAeEq@qBi@oJuCaIiDsCmAmOwE{QiGsIcF{Y}Rs_@gXcNuJsKwGkGwEkBiCiFwJaB{AiBo@kJq@}FQaNn@abAnGgg@|BmLRiEa@uGkBuDuBqDeDyKeP_CsC}DyBaXqLcEsCwBuCkAmCeDmOiG}XoBoD}CsCoCuAcE_AqR_@qR?sYn@cd@|@eEGih@aJeIm@qSc@cTm@ed@eCiDi@qMqGyUyMcMmF{F{@}EOgd@QuCU_G{AkF_DmM{P_VqYqSqX_Xw]qHeJaEaD_L{EoHgAiNe@yYs@kS_@ec@aAoFcAiEmCuCsE_JsSmNoUkEuEaHaDuIu@sGLe@Z]hAd@bAnAPxAiAFgN@eG]_EgCkDcD_D{DcG}@{C?cI_@aE}Qs[a@iEOoF_DiGeHgLiEaLkCsOcF{OyB_EcDwEB_PkC{EeBgGw@eDc@gBs@k@wAy@gAuDqN~@}E\\Bn@APA@"
     );
+    destinations = this.props.destinations;
     polyline_coords = latlngs;
-    console.log(polyline_coords);
-    this.setState({ polyline_coords: latlngs });
+    // let temp = [];
+    // for (let i = 0; i < this.props.destinations.length; i++) {
+    //   console.log(this.props.destinations[i].lat);
+    //   temp.push({
+    //     lat: this.props.destinations[i].lat,
+    //     lng: this.props.destinations[i].lng,
+    //   });
+    // }
+    // destinations = temp;
+    // console.log(temp);
   }
 
-  drawMarker = (index) => {
-    if (polyline_coords.length === 0) return;
-    const latlngs = polyline_coords;
+  drawMarker = (data, index) => {
+    if (this.state.destinations.length === 0) return;
+    const latlngs = this.state.destinations;
     return (
       <Marker
         key={index}
         id={index}
         position={{
-          lat: latlngs[index][0],
-          lng: latlngs[index][1],
+          lat: latlngs[index].lat,
+          lng: latlngs[index].lng,
         }}
         onClick={() => console.log("Event Handler Called")}
       />
     );
+  };
+
+  getMarkers = () => {
+    return this.state.destinations.map(this.drawMarker);
   };
 
   getPolyLine = () => {
@@ -78,8 +83,21 @@ class GoogleMapComponent extends React.Component {
         }}
       >
         {this.getPolyLine()}
-        {this.drawMarker(0)}
-        {this.drawMarker(polyline_coords.length - 1)}
+        {destinations !== [] &&
+          destinations.map((data, index) => {
+            console.log(data);
+            return (
+              <Marker
+                key={index}
+                id={index}
+                position={{
+                  lat: data.lat,
+                  lng: data.lng,
+                }}
+                onClick={() => console.log("Event Handler Called")}
+              />
+            );
+          })}
       </Map>
     );
   }
