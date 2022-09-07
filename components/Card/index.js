ea430e607d25bad626808120a17c30f2e4ff6820
Shivam Akhouri <shivamakhouri1142@gmail.com>
2022-09-08 00:33:45 +0530

    added card component

---
+++ components/Card/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Card(props) {
+  return (
+    <div class="flex justify-center">
+  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+  </div>
+</div>
+  )
+}
