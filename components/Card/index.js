c226779b13c7160bf1e58830583a935507f382ed
Shivam Akhouri <shivamakhouri1142@gmail.com>
2022-09-08 01:10:43 +0530

    aadha kaam

--- components/Card/index.js
+++ components/Card/index.js
@@ -3,8 +3,9 @@ import Image from 'next/image'
 
 export default function Card(props) {
   return (
-    <div class="flex justify-center">
-  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+    <div class="flex justify-center w-[100%] mb-1">
+  <div class="block w-[95%] p-3 rounded-lg shadow-lg bg-[#212638] text-white font-black">
+    {props.children}
   </div>
 </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,57 @@ import Card from '../components/Card'
 
 export default function Home() {
   return (
-    <Card></Card>
+    <div className='flex bg-[#2F3855]'>
+      <div class="flex-none w-[20vw] h-[100vh]">
+        <div className='h-[13vh] bg-[#ffbd59]'></div>
+        <div className='overflow-scroll max-h-[90vh]'> 
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>Agent 1</Card>
+          <Card>
+            Agent 2
+          </Card>
+          <Card>
+            Agent 3
+          </Card>
+          <Card>
+            Agent 4
+          </Card>
+        </div>
+      </div>
+      <div class="flex-none w-[80vw] h-[100vh]">
+        <div className='flex h-[10vh] mt-7'>
+            <Card>
+            <p className='font-normal text-gray-300'>Average Time</p>
+            <p className='text-white font-black'>Dummy Text</p> 
+            </Card>
+            <Card>
+            <p className='font-normal text-gray-300'>Distance Travelled</p>
+            <p className='text-white font-black'>Dummy Text</p> 
+            </Card>
+            <Card>
+            <p className='font-normal text-gray-300'>Conversion Rate</p>
+            <p className='text-white font-black'>Dummy Text</p> 
+            </Card>
+            <Card>
+            <p className='font-normal text-gray-300'>Current Temperature</p>
+            <p className='text-white font-black'>Dummy Text</p> 
+            </Card>
+            <Card>
+            <p className='font-normal text-gray-300'>Travel Time</p>
+            <p className='text-white font-black'>Dummy Text</p> 
+            </Card>
+        </div>
+        <div className='h-[90vh]'>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
